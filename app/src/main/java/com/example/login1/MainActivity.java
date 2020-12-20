1b41e7d8baf96818925ecbc72abf0e7b54e086f2
Jeyanivedita <nivedita187@gmail.com>
2020-12-20 23:12:34 +0530

    first commit

---
+++ app/src/main/java/com/example/login1/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.login1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.action_setting:
+                //settings
+
+                Intent settings = new Intent(this, AccountActivity.class);
+                startActivity(settings);
+                break;
+
+            default:
+                // select an option
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
